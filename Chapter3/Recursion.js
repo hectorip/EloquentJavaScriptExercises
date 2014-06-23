7a8bae35ccca5835ecbc8adabd587c33dac2bf6e
hectorip <hectorivanpatriciomoreno@gmail.com>
2014-06-23 13:16:15 -0500

    Chapter 3 exercises

---
+++ Chapter3/Recursion.js
@@ -0,0 +1,20 @@
+/**
+ * We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to check if it’s divisible by two. 
+ * Here’s another way to define whether a positive whole number is even or odd:
+ *  - Zero is even.
+ *  - One is odd.
+ *  - For any other number N, its evenness is the same as N - 2.
+ *  
+ *  Define a recursive function isEven corresponding to this description. The function should accept a number parameter and return a boolean.
+ */
+
+function isEven(num) {
+    if(num < 0)
+		num = -num;
+	if(num === 0)
+		return true;
+	else if(num === 1)
+		return false;
+	else
+		return isEven(num-2);
+}
